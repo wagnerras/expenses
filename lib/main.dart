a803cf1946bbb4dc61aedc7241c37d78ea4838b6
Wagner Ribeiro <wagnerras@gmail.com>
2021-03-18 10:37:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import './components/transaction_user.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Despesas Pessoais')),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                child: Text('Gráfico'),
+                elevation: 5,
+                color: Colors.blue,
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ));
+  }
+}
